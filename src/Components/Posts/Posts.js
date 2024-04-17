e0230a318dc0fca9c86d87fab61b8b858978a335
hudhyfa <hudhyfahbk@gmail.com>
2024-04-17 19:28:06 +0530

    Made posts dynamic

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,9 +1,26 @@
-import React from 'react';
-
-import Heart from '../../assets/Heart';
-import './Post.css';
+import React, { useState, useEffect } from "react";
+import { FirebaseContextHook } from "../../store/Context";
+import Heart from "../../assets/Heart";
+import "./Post.css";
 
 function Posts() {
+  const { firebase } = FirebaseContextHook();
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("products")
+      .get()
+      .then((snapshot) => {
+        const allPosts = snapshot.docs.map((product) => {
+          return {
+            ...product.data(),
+            id: product.id,
+          };
+        });
+        setProducts(allPosts);
+      });
+  }, []);
 
   return (
     <div className="postParentDiv">
@@ -13,24 +30,26 @@ function Posts() {
           <span>View more</span>
         </div>
         <div className="cards">
-          <div
-            className="card"
-          >
-            <div className="favorite">
-              <Heart></Heart>
-            </div>
-            <div className="image">
-              <img src="../../../Images/R15V3.jpg" alt="" />
-            </div>
-            <div className="content">
-              <p className="rate">&#x20B9; 250000</p>
-              <span className="kilometer">Two Wheeler</span>
-              <p className="name"> YAMAHA R15V3</p>
-            </div>
-            <div className="date">
-              <span>Tue May 04 2021</span>
-            </div>
-          </div>
+          {products.map((product) => {
+            return (   
+              <div className="card" key={product.id}>
+                <div className="favorite">
+                  <Heart></Heart>
+                </div>
+                <div className="image">
+                  <img src={product.url} alt="product-img" />
+                </div>
+                <div className="content">
+                  <p className="rate">&#x20B9; {product.price}</p>
+                  <span className="kilometer">{product.category}</span>
+                  <p className="name"> {product.name}</p>
+                </div>
+                <div className="date">
+                  {/* <span>{product.createdAt}</span> */}
+                </div>
+              </div>
+            )
+          })}
         </div>
       </div>
       <div className="recommendations">
